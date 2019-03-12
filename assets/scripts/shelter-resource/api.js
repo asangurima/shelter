dd0e92eca4ec1d0f3d548a32d0a1384252a754ca
asangurima <asangurima@gmail.com>
2019-03-12 15:43:39 -0400

    Add draft of AJAX requests for shelter-resource

--- assets/scripts/shelter-resource/api.js
+++ assets/scripts/shelter-resource/api.js
@@ -1,53 +1,85 @@
 
-SHELTER
+// SHELTER
 
-// 'use strict'
+'use strict'
+
+const config = require('../config.js')
+const store = require('../store.js')
 //
-// const config = require('../config.js')
-// const store = require('../store.js')
-//
-// const signUp = (formData) => {
+// const createShelter = () => {
 //   return $.ajax({
-//     url: config.apiUrl + '/sign-up',
+//     url: config.apiUrl + '/shelters',
 //     method: 'POST',
-//     data: formData
-//   })
-// }
-//
-// const signIn = (formData) => {
-//   return $.ajax({
-//     url: config.apiUrl + '/sign-in',
-//     method: 'POST',
-//     data: formData
-//   })
-// }
-//
-// const changePassword = (formData) => {
-//   // console.log('store:', store)
-//   return $.ajax({
-//     url: config.apiUrl + '/change-password',
-//     method: 'PATCH',
 //     headers: {
 //       Authorization: 'Token token=' + store.user.token
 //     },
-//     data: formData
+//     data: ''
 //   })
 // }
-//
-// const signOut = (formData) => {
-//   // console.log('store:', store)
+
+const getShelters = () => {
+  return $.ajax({
+    url: config.apiUrl + '/shelters',
+    method: 'GET'
+  })
+}
+
+// Do i need this to just get shelters belonging to signed in user?
+// const getUserShelters = () => {
 //   return $.ajax({
-//     url: config.apiUrl + '/sign-out',
-//     method: 'DELETE',
+//     url: config.apiUrl + '/shelters',
+//     method: 'GET',
 //     headers: {
 //       Authorization: 'Token token=' + store.user.token
 //     }
 //   })
 // }
-//
-// module.exports = {
-//   signUp: signUp,
-//   signIn: signIn,
-//   changePassword: changePassword,
-//   signOut: signOut
+
+// how do I pass in the shelter id?
+// const getShelter = () => {
+//   return $.ajax({
+//     url: config.apiUrl + `/shelters/${store.shelter.id}`,
+//     method: 'GET',
+//     headers: {
+//       Authorization: 'Token token=' + store.user.token
+//     }
+//   })
 // }
+
+// const updateShelter = (name, location, phone, beds) => {
+//   return $.ajax({
+//     url: config.apiUrl + `/shelters/${store.shelter.id}`,
+//     method: 'PATCH',
+//     headers: {
+//       Authorization: 'Token token=' + store.user.token
+//     },
+//     data: {
+//       'shelter': {
+//         'name': name,
+//         'location': location,
+//         'phone_num': phone,
+//         'avail_beds': beds
+//       }
+//     }
+//   })
+// }
+
+const deleteShelter = (shelterId) => {
+  // console.log('store:', store)
+  return $.ajax({
+    url: config.apiUrl + `/shelters/` + shelterId,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  // createShelter,
+  getShelters,
+  // getUserShelters,
+  // getShelter,
+  // updateShelter,
+  deleteShelter
+}
