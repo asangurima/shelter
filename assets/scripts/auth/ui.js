c9ee1832cf3796110f260996725ff006d3941ebc
asangurima <asangurima@gmail.com>
2019-03-12 13:38:50 -0400

    Add forms & functionality for sign-up & sign-in
    -Add forms for sign-up, sign-in
    -Add ability to sign-up, sign in
    -Add styling to nav bar
    -Add navbar sign-out view vs. sign-in view

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,54 +1,55 @@
-// 'use strict'
-// const store = require('../store.js')
-//
-// const signUpSuccess = () => {
-//   $('.auth-messages-2').text('Successfully signed up!')
-//   $('form').trigger('reset')
-//   setTimeout(() => {
-//     $('.auth-messages-2').html('')
-//   }, 3000)
-// }
-//
-// const signUpFailure = () => {
-//   $('.auth-messages-2').text('Something went wrong. Perhaps try a different email.')
-//   $('form').trigger('reset')
-//   setTimeout(() => {
-//     $('.auth-messages-2').html('')
-//   }, 3000)
-// }
-//
-// const signInSuccess = (responseData) => {
-//   $('.auth-messages').text(`Successfully signed in!`)
-//   setTimeout(() => {
-//     $('.auth-messages').text(` Hello, ${responseData.user.email}!`)
-//   }, 3000)
-//   // this should make the sign out button display on succesful sign-in
-//   $('#sign-out-form').removeClass('d-none')
-//   $('#resetGame').removeClass('d-none')
-//   $('#number-of-games').removeClass('d-none')
-//   // $('.userMessages').html('x starts the game!')
-//   $('.userMessages').html('')
-//   const revealBoard = () => {
-//     $('.game-board').removeClass('d-none')
-//     $('.userMessages').html('x starts the game!')
-//   }
-//   $('#resetGame').on('click', revealBoard)
-//   $('#change-pw-button').removeClass('d-none')
-//   // this should make the sign in form disappear on succesful sign-in
-//   $('#sign-in-form').addClass('d-none')
-//   $('#signUpButton').addClass('d-none')
-//   // save the token
-//   store.user = responseData.user
-// }
-//
-// const signInFailure = () => {
-//   $('.auth-messages').text('Something went wrong, please try again!')
-//   $('form').trigger('reset')
-//   setTimeout(() => {
-//     $('.auth-messages').html('')
-//   }, 3000)
-// }
-//
+'use strict'
+const store = require('../store.js')
+
+const signUpSuccess = () => {
+  $('.register-msg').text('Successfully registered!')
+  $('form').trigger('reset')
+  setTimeout(() => {
+    $('.register-msg').html('')
+  }, 3000)
+}
+
+const signUpFailure = () => {
+  $('.register-msg').text('Something went wrong. Try a different email.')
+  $('form').trigger('reset')
+  setTimeout(() => {
+    $('.register-msg').html('')
+  }, 3000)
+}
+
+const signInSuccess = (responseData) => {
+  $('.user-messages').text(`Successfully signed in!`)
+  setTimeout(() => {
+    $('.user-messages').text(` Hello, ${responseData.user.email}!`)
+  }, 3000)
+  // this should make the sign out button display on succesful sign-in
+  $('.modal-sign-in').modal('toggle')
+  $('.navbar-collapse').collapse('hide')
+  $('.signed-out-nav').addClass('d-none')
+  $('.signed-in-nav').removeClass('d-none')
+  // $('.userMessages').html('x starts the game!')
+  // $('.userMessages').html('')
+  // const revealBoard = () => {
+  //   $('.game-board').removeClass('d-none')
+  //   $('.userMessages').html('x starts the game!')
+  // }
+  // $('#resetGame').on('click', revealBoard)
+  // $('#change-pw-button').removeClass('d-none')
+  // this should make the sign in form disappear on succesful sign-in
+  // $('#sign-in-form').addClass('d-none')
+  // $('#signUpButton').addClass('d-none')
+  // save the token
+  store.user = responseData.user
+}
+
+const signInFailure = () => {
+  $('.sign-in-msg').text('Something went wrong, please try again!')
+  $('form').trigger('reset')
+  setTimeout(() => {
+    $('.sign-in-msg').html('')
+  }, 3000)
+}
+
 // const changePasswordSuccess = () => {
 //   $('.password-messages').text('Successfully updated password.')
 //   $('form').trigger('reset')
@@ -89,13 +90,13 @@
 //   }, 3000)
 // }
 //
-// module.exports = {
-//   signUpSuccess,
-//   signUpFailure,
-//   signInSuccess,
-//   signInFailure,
-//   changePasswordSuccess,
-//   changePasswordFailure,
-//   signOutSuccess,
-//   signOutFailure
-// }
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure
+  // changePasswordSuccess,
+  // changePasswordFailure,
+  // signOutSuccess,
+  // signOutFailure
+}
---
+++ assets/scripts/shelter-resource/ui.js
@@ -0,0 +1,106 @@
+
+SHELTER
+
+// 'use strict'
+// const store = require('../store.js')
+//
+// const signUpSuccess = () => {
+//   console.log('sign up success- woohoo!')
+//   $('.register-msg').text('Successfully registered!')
+//   $('form').trigger('reset')
+//   setTimeout(() => {
+//     $('.register-msg').html('')
+//   }, 3000)
+// }
+//
+// const signUpFailure = () => {
+//   console.log('sign up failure')
+//   $('.register-msg').text('Something went wrong. Perhaps try a different email.')
+//   $('form').trigger('reset')
+//   setTimeout(() => {
+//     $('.register-msg').html('')
+//   }, 3000)
+// }
+
+// const signInSuccess = (responseData) => {
+//   $('.auth-messages').text(`Successfully signed in!`)
+//   setTimeout(() => {
+//     $('.auth-messages').text(` Hello, ${responseData.user.email}!`)
+//   }, 3000)
+//   // this should make the sign out button display on succesful sign-in
+//   $('#sign-out-form').removeClass('d-none')
+//   $('#resetGame').removeClass('d-none')
+//   $('#number-of-games').removeClass('d-none')
+//   // $('.userMessages').html('x starts the game!')
+//   $('.userMessages').html('')
+//   const revealBoard = () => {
+//     $('.game-board').removeClass('d-none')
+//     $('.userMessages').html('x starts the game!')
+//   }
+//   $('#resetGame').on('click', revealBoard)
+//   $('#change-pw-button').removeClass('d-none')
+//   // this should make the sign in form disappear on succesful sign-in
+//   $('#sign-in-form').addClass('d-none')
+//   $('#signUpButton').addClass('d-none')
+//   // save the token
+//   store.user = responseData.user
+// }
+//
+// const signInFailure = () => {
+//   $('.auth-messages').text('Something went wrong, please try again!')
+//   $('form').trigger('reset')
+//   setTimeout(() => {
+//     $('.auth-messages').html('')
+//   }, 3000)
+// }
+//
+// const changePasswordSuccess = () => {
+//   $('.password-messages').text('Successfully updated password.')
+//   $('form').trigger('reset')
+//   setTimeout(() => {
+//     $('.password-messages').html('')
+//   }, 3000)
+// }
+//
+// const changePasswordFailure = () => {
+//   $('.password-messages').text('Something went wrong, please try again!')
+//   $('form').trigger('reset')
+//   setTimeout(() => {
+//     $('.password-messages').html('')
+//   }, 3000)
+// }
+//
+// const signOutSuccess = () => {
+//   $('.auth-messages').text('Successfully signed out.')
+//   $('form').trigger('reset')
+//   $('#number-of-games').addClass('d-none')
+//   $('.game-board').addClass('d-none')
+//   $('#resetGame').addClass('d-none')
+//   $('#change-pw-button').addClass('d-none')
+//   $('#sign-in-form').removeClass('d-none')
+//   $('#sign-out-form').addClass('d-none')
+//   $('.userMessages').html('Please sign-in to play.')
+//   $('#signUpButton').removeClass('d-none')
+//   store.user = null
+//   setTimeout(() => {
+//     $('.auth-messages').html('')
+//   }, 3000)
+// }
+//
+// const signOutFailure = () => {
+//   $('.auth-messages').text('Something went wrong, please try again!')
+//   setTimeout(() => {
+//     $('.auth-messages').html('')
+//   }, 3000)
+// }
+//
+module.exports = {
+  signUpSuccess,
+  signUpFailure
+  // signInSuccess,
+  // signInFailure,
+  // changePasswordSuccess,
+  // changePasswordFailure,
+  // signOutSuccess,
+  // signOutFailure
+}
