439f9a2b921347a87e5b1d5ddd4632699940d072
asangurima <asangurima@gmail.com>
2019-03-14 16:42:45 -0400

    Remove weird brackets from url in /config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<https://gimmeshelter.herokuapp.com>',
+  production: 'https://gimmeshelter.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
